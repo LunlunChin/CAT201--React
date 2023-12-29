2d4765e45a0a54afc2754032f8f859693d4ddaa2
Sidneyran <ranzhao0113@gmail.com>
2023-12-29 12:53:02 +0800

    add team member photos

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import teammember1 from "../assets/team-member-1.jpg";
+import teammember2 from "../assets/team-member-2.jpg";
+import teammember3 from "../assets/team-member-3.jpg";
+
 
 const Team: React.FC = () => {
   return (
@@ -8,22 +12,22 @@ const Team: React.FC = () => {
         <div className="row">
           <div className="col-md-4">
             <div className="team-member">
-              <img src="team-member-1.jpg" alt="Team Member 1" />
-              <h3>John Doe</h3>
+            <img src={teammember1} className="card-img-top" alt="Team Member 1" />
+              <h3>CHIN ZHIE LUN</h3>
               <p>CEO</p>
             </div>
           </div>
           <div className="col-md-4">
             <div className="team-member">
-              <img src="team-member-2.jpg" alt="Team Member 2" />
-              <h3>Jane Smith</h3>
+              <img src={teammember2} className="card-img-top" alt="Team Member 2" />
+              <h3>ZHAO RAN</h3>
               <p>CTO</p>
             </div>
           </div>
           <div className="col-md-4">
             <div className="team-member">
-              <img src="team-member-3.jpg" alt="Team Member 3" />
-              <h3>Mike Johnson</h3>
+            <img src={teammember3} className="card-img-top" alt="Team Member 3" />
+              <h3>YANG QU</h3>
               <p>Lead Developer</p>
             </div>
           </div>
